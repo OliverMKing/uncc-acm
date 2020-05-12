f33b27d5c71bd054631926d36986e262a2c803da
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-05-12 16:24:13 -0400

    Added AWS Amplify

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,11 @@ import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
