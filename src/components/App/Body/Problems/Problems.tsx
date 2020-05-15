42f4b9b8597e835cb6be93689762fbf28d9df605
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-05-14 21:58:08 -0400

    Added problem filter and search

--- src/components/App/Body/Problems/Problems.tsx
+++ src/components/App/Body/Problems/Problems.tsx
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Typography } from "@material-ui/core";
+import {
+  Typography,
+  TextField,
+  InputAdornment,
+  IconButton,
+  Button,
+  createStyles,
+  Theme,
+  Grid,
+  Popover,
+  makeStyles,
+} from "@material-ui/core";
+
+import { Search, FilterList } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    searchAndFilters: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-end",
+    },
+    filterPopup: {
+      padding: theme.spacing(2),
+    },
+  })
+);
 
 const Problems = () => {
+  const classes = useStyles();
+
+  // State for search input
+  const [search, setSearch] = useState("");
+
+  const handleSearchChange = (event: any): void => {
+    setSearch(event.target.value);
+  };
+
+  // State for filter dropdown
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleFilterClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleFilterClose = () => {
+    setAnchorEl(null);
+  };
+
+  const filterOpen = Boolean(anchorEl);
+
   return (
     <div>
       <Typography variant="h3" component="h3" align="left" gutterBottom>
@@ -14,6 +64,52 @@ const Problems = () => {
         with. You can search for different kinds of problems below. Signed in
         users can upload their solutions.
       </Typography>
+      <div className={classes.searchAndFilters}>
+        <TextField
+          placeholder="Search"
+          value={search}
+          onChange={handleSearchChange}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <IconButton disabled>
+                  <Search />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <IconButton
+          onClick={handleFilterClick}
+          color={filterOpen ? "primary" : "default"}
+        >
+          <FilterList />
+        </IconButton>
+        <Popover
+          open={filterOpen}
+          anchorEl={anchorEl}
+          onClose={handleFilterClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <div className={classes.filterPopup}>
+            <Grid alignItems="center" container spacing={5}>
+              <Grid item xs={4}>
+                <Typography variant="h6">Filters</Typography>
+              </Grid>
+              <Grid item xs={8}>
+                <Button color="primary">Reset</Button>
+              </Grid>
+            </Grid>
+          </div>
+        </Popover>
+      </div>
     </div>
   );
 };
