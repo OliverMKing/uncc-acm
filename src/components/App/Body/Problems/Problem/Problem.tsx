943b4d24ea6efa3d2076df37686adfaddc9b0659
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-05-18 14:54:29 -0400

    Problem page displays information about problems

--- src/components/App/Body/Problems/Problem/Problem.tsx
+++ src/components/App/Body/Problems/Problem/Problem.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import { CircularProgress } from "@material-ui/core";
+import { CircularProgress, Typography, Link, Chip } from "@material-ui/core";
 
 import { API, graphqlOperation } from "aws-amplify";
 import { getProblem } from "../../../../../graphql/queries";
@@ -34,7 +34,42 @@ const Problem = (props: any) => {
 
   return (
     <div>
-      <h1>{problem.name}</h1>
+      <Typography variant="h3" component="h3" align="left">
+        {problem.name}
+      </Typography>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "left",
+          margin: "10px 0px 10px 0px",
+        }}
+      >
+        {problem.tags.map((tag: String) => {
+          return (
+            <Chip
+              variant="outlined"
+              color="primary"
+              label={tag}
+              style={{ marginRight: "10px" }}
+            />
+          );
+        })}
+      </div>
+      <Link href={problem.link} target="_blank" rel="noopener noreferrer">
+        <Typography variant="subtitle1" align="left" gutterBottom>
+          {problem.link}
+        </Typography>
+      </Link>
+      <Typography variant="subtitle1" align="left" gutterBottom>
+        Difficulty {problem.difficulty}
+      </Typography>
+
+      <Typography variant="body1" align="left" gutterBottom paragraph>
+        We work on different problems every week. This page is a collection of
+        problems we have previously worked on and solutions we have come up
+        with. You can search for different kinds of problems below. Signed in
+        users can upload their solutions.
+      </Typography>
     </div>
   );
 };
