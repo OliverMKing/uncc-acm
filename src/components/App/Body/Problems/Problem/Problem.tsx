f8d3854be1f3396bc801578f4f10c018e46779e1
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-05-17 21:02:33 -0400

    Fetch problem based on id

--- src/components/App/Body/Problems/Problem/Problem.tsx
+++ src/components/App/Body/Problems/Problem/Problem.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { CircularProgress } from "@material-ui/core";
+
+import { API, graphqlOperation } from "aws-amplify";
+import { getProblem } from "../../../../../graphql/queries";
 
 const Problem = (props: any) => {
+  const [problem, setProblem] = useState<any>(null);
+
+  async function fetchProblem() {
+    try {
+      const problemData: any = await API.graphql(
+        graphqlOperation(getProblem, { id: props.match.params.id })
+      );
+      setProblem(problemData.data.getProblem);
+      console.log(problemData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProblem();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (problem === null)
+    return (
+      <div style={{ margin: "200px 0px 200px 0px" }}>
+        <CircularProgress />
+      </div>
+    );
+
   return (
     <div>
-      <h1>{props.match.params.id}</h1>
+      <h1>{problem.name}</h1>
     </div>
   );
 };
