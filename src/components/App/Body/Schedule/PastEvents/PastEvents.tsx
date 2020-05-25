6863f64286f92b199c780e7d3a679dc32f5971b5
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-05-24 23:46:06 -0400

    Added past event content

--- src/components/App/Body/Schedule/PastEvents/PastEvents.tsx
+++ src/components/App/Body/Schedule/PastEvents/PastEvents.tsx
@@ -5,9 +5,19 @@ import {
   ExpansionPanelSummary,
   ExpansionPanelDetails,
   Typography,
+  Grid,
 } from "@material-ui/core";
 
 import { ExpandMore } from "@material-ui/icons";
+
+import Team1_2019 from "../../../../../images/ICPC/2019Team1.jpg";
+import Team2_2019 from "../../../../../images/ICPC/2019Team2.jpg";
+import Team3_2019 from "../../../../../images/ICPC/2019Team3.jpg";
+import Team4_2019 from "../../../../../images/ICPC/2019Team4.jpg";
+import Team1_2018 from "../../../../../images/ICPC/2018Team1.jpg";
+import Team2_2018 from "../../../../../images/ICPC/2018Team2.jpg";
+import Team3_2018 from "../../../../../images/ICPC/2018Team3.jpg";
+import Team4_2018 from "../../../../../images/ICPC/2018Team4.jpg";
 
 // Define events here as components
 const ICPC2019 = () => {
@@ -17,6 +27,73 @@ const ICPC2019 = () => {
         All our teams had a great showing by completeing at least three
         problems. Our top team placed 14th out of 160 teams.
       </Typography>
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <img
+            src={Team1_2019}
+            style={{ width: "100%" }}
+            alt="2019 ICPC Team"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src={Team2_2019}
+            style={{ width: "100%" }}
+            alt="2019 ICPC Team"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src={Team3_2019}
+            style={{ width: "100%" }}
+            alt="2019 ICPC Team"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src={Team4_2019}
+            style={{ width: "100%" }}
+            alt="2019 ICPC Team"
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+const ICPC2018 = () => {
+  return (
+    <div>
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <img
+            src={Team1_2018}
+            style={{ width: "100%" }}
+            alt="2019 ICPC Team"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src={Team2_2018}
+            style={{ width: "100%" }}
+            alt="2019 ICPC Team"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src={Team3_2018}
+            style={{ width: "100%" }}
+            alt="2019 ICPC Team"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src={Team4_2018}
+            style={{ width: "100%" }}
+            alt="2019 ICPC Team"
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 };
@@ -28,8 +105,12 @@ interface IEvent {
 
 const Events: IEvent[] = [
   {
-    name: "ICPC Mid-Atlantic Regional Programming Contest (11/9/2019)",
+    name: "2019 ICPC Mid-Atlantic Regional Programming Contest",
     body: ICPC2019,
+  },
+  {
+    name: "2018 ICPC Mid-Atlantic Regional Programming Contest",
+    body: ICPC2018,
   },
 ];
 
